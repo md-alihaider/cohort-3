15eb6e869160b29bdf48554cae4938234fbd4b30
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-05-29 16:03:37 +0530

    feat: Add initial HTML and JavaScript files for practice exercises

---
+++ JavaScriptPractice/script.js
@@ -0,0 +1,95 @@
+
+// Beginner Level 
+//1 Create a function named greet that prints "Hello World".
+
+// function greet() {
+//   console.log("Hello World");
+// }
+
+// greet();
+
+//2 Create a function add(a, b) that returns the sum.
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(23, 56));
+// console.log(add(2, 56));
+
+//3 Write a function to calculate the square of a number.
+
+// function sqOfNumber(num) {
+//   return num * num;
+// }
+
+// console.log(sqOfNumber(4));
+
+//4 Create a function that checks whether a number is even or odd.
+
+// function checkEvenOdd(num) {
+//   if (num % 2 === 0) {
+//     return "Even";
+//   }
+//   return "Odd";
+// }
+
+// console.log(checkEvenOdd(578));
+
+//5 Write a function that converts Celsius to Fahrenheit.
+
+// function celsiusToFahrenheit(c) {
+//   let f = (c * 9) / 5 + 32;
+//   return f;
+// }
+
+// let farenheit = celsiusToFahrenheit(25);
+// console.log(`${farenheit} F`);
+
+//6 Create a function with default parameter "Guest".
+
+// function greet(user = "Guest") {
+//   console.log(`Good morning, ${user}`);
+// }
+
+// greet("Ali");
+// greet();
+
+//7 Write a function that returns the greater of two numbers.
+
+// function greaterOfTwoNum(num1, num2) {
+//   return num1 > num2 ? num1 : num2;
+// }
+
+// console.log(greaterOfTwoNum(35, 6));
+// console.log(greaterOfTwoNum(7, 6));
+
+//8 Create a function to calculate area of rectangle.
+
+// function calAreaOfRectangle(l, b) {
+//   return 2 * (l + b);
+// }
+
+// console.log(calAreaOfRectangle(3, 15));
+
+//9 Write a function that returns "Adult" if age ≥ 18 else "Minor".
+
+// function checkAdultOrMinor(age) {
+//   return age >= 18 ? "Adult" : "Minor";
+// }
+
+// console.log(checkAdultOrMinor(35));
+
+//10 Create a function to reverse a string.
+
+// function reverseString(word) {
+//   let str = "";
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     str += word[i];
+//   }
+//   return str;
+// } 
+// const reverseString = (word) => [...word].reverse().join("");
+
+// console.log(reverseString("Love"));
+
+// console.log(reverseString("Love"));
