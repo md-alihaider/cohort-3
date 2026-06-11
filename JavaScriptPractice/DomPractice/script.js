c04e7709a5bfd3b48904941fe27a8e99bd992ca2
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-11 18:13:46 +0530

    feat: Update button text and add lightup effect for bulb toggle

--- JavaScriptPractice/DomPractice/script.js
+++ JavaScriptPractice/DomPractice/script.js
@@ -1,16 +1,23 @@
-
 const bulb = document.querySelector(".bulb");
 const btn = document.querySelector("button");
 
-let flag = true;
-btn.addEventListener("click", function () {
-  if (flag) {
-    bulb.style.backgroundColor = "yellow";
+// let flag = true;
+// btn.addEventListener("click", function () {
+//   if (flag) {
+//     bulb.style.backgroundColor = "yellow";
+//     btn.innerText = "Turn Off";
+//     flag = false;
+//   } else {
+//     bulb.style.backgroundColor = "black";
+//     btn.innerText = "Turn On";
+//     flag = true;
+//   }
+// });
+
+btn.addEventListener("click", () => {
+  if (bulb.classList.toggle("lightup")) {
     btn.innerText = "Turn Off";
-    flag = false;
   } else {
-    bulb.style.backgroundColor = "black";
     btn.innerText = "Turn On";
-    flag = true;
   }
 });
