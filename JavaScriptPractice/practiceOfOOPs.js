fb0a3b0081f04b72b223b0800a6ae4b874fea463
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-12 21:35:23 +0530

    feat: Add comments for understanding 'this' and default binding in practiceOfOOPs.js

--- JavaScriptPractice/practiceOfOOPs.js
+++ JavaScriptPractice/practiceOfOOPs.js
@@ -7,3 +7,39 @@
 //   },
 // };
 // user.greet(); // Ritik
+
+// Question 2 — Default Binding
+// function show() {
+//   console.log(this);
+// }
+// show();
+
+// Browser /window
+// Node.js /global context
+// Strict Mode /undefined
+
+
+
+
+// Question 5 — Fix Lost this
+// const user = {
+//   name: "Ritik",
+//   greet() {
+//     console.log(this.name);
+//   },
+// };
+
+// // const fn = user.greet;
+// const fn = user.greet.bind(user);
+// fn();
+
+
+
+// Question 6 — Create an Inheritance Chain
+// const animal = {
+//   eats: true,
+// };
+
+// const dog = Object.create(animal);
+// dog.barks = true;
+// console.log(dog.eats); // true
