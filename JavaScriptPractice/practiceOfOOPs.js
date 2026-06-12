9ca27ca7cb90d498fa60f420ac812d6db7a57102
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-12 21:58:23 +0530

    feat: Add prototype method example and demonstrate inheritance in practiceOfOOPs.js

--- JavaScriptPractice/practiceOfOOPs.js
+++ JavaScriptPractice/practiceOfOOPs.js
@@ -43,3 +43,20 @@
 // const dog = Object.create(animal);
 // dog.barks = true;
 // console.log(dog.eats); // true
+
+
+// Question 7 — Prototype Method
+// function Person(name) {
+//   this.name = name;
+// }
+
+
+// Person.prototype.greet = function () {
+//   console.log(`Hello, ${this.name}`);
+// };
+
+// const person1 = new Person("Ritik");
+// const person2 = new Person("Aman");
+
+// person1.greet(); // Hello, Ritik
+// person2.greet(); // Hello, Aman
