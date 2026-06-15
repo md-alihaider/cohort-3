7d54841796169121e98fbdbde2eb1980381c43ef
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-15 22:24:04 +0530

    feat: Enhance To-Do List functionality with item creation and editing features

--- JavaScriptPractice/DomPractice/to-doList/script.js
+++ JavaScriptPractice/DomPractice/to-doList/script.js
@@ -2,16 +2,38 @@ const inp = document.querySelector("input");
 const btn = document.querySelector("#add-btn");
 const todoBox = document.querySelector(".todo-list");
 
+
 btn.addEventListener("click", () => {
+  const li = document.createElement("li");
   const value = inp.value;
-  if(value.trim() === "") return;
-  todoBox.innerHTML += ` <div class="li">
+  if (value.trim() === "") return;
+  li.innerHTML = ` 
         <h3>${value}</h3>
         <div>
           <button class="btn edit">Edit</button>
           <button class="btn delete">Delete</button>
         </div>
-      </div>`;
-  
+      `;
+  li.classList.add("li");
+  todoBox.appendChild(li);
   inp.value = "";
 });
+
+
+todoBox.addEventListener("click", (e) => {
+  const clickedElement = e.target;
+  const currentTodo = clickedElement.closest("li");
+
+  if(clickedElement.classList.contains("delete")) {
+    currentTodo.remove();
+  }
+
+
+  if (clickedElement.classList.contains("edit")) {
+    const heading = currentTodo.querySelector("h3");
+    const newValue = prompt("Edit your task:", heading.textContent);
+    if (newValue !== null && newValue.trim() !== "") {
+      heading.textContent = newValue;
+    }
+  }
+})
