c03bdb341b4579ff7a2ab9b6165725fa64828eb3
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-17 16:04:08 +0530

    feat: Add initial HTML, CSS, and JavaScript files for Interactive Task Manager

--- Projects/e-commerce/script.js
+++ Projects/e-commerce/script.js
@@ -1,6 +1,7 @@
 const createBtn = document.querySelector("#create");
 const createProduct = document.querySelector(".create-product");
 const closeBtn = document.querySelector("#cancel");
+const form = document.querySelector("form");
 
 createBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -10,4 +11,10 @@ createBtn.addEventListener("click", (e) => {
 closeBtn.addEventListener("click", (e) => {
   e.preventDefault();
   createProduct.style.display = "none";
+})
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const product = e.target[0].value;
+  console.log(product);
 })
