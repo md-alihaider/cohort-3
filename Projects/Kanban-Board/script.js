6901f880ef8907223f743b2d9042608a3c33b329
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-22 19:28:17 +0530

    feat: Add IDs to task columns and implement drag-and-drop functionality with hover effect

---
+++ Projects/Kanban-Board/script.js
@@ -0,0 +1,27 @@
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    // console.log("dragging", e);
+  });
+});
+
+function addDragEventOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+  column.addEventListener("dragleave", (e) => {
+    column.classList.remove("hover-over");
+  });
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+  });
+}
+
+addDragEventOnColumn(todo);
+addDragEventOnColumn(progress);
+addDragEventOnColumn(done);
