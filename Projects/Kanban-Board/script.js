62162e550283f583b948306666de8c139ab91f0d
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-22 20:05:46 +0530

    feat: Implement modal for adding new tasks with styling and drag-and-drop functionality

--- Projects/Kanban-Board/script.js
+++ Projects/Kanban-Board/script.js
@@ -2,10 +2,11 @@ const todo = document.querySelector("#todo");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
 const tasks = document.querySelectorAll(".task");
+let draggedTask = null;
 
 tasks.forEach((task) => {
   task.addEventListener("drag", (e) => {
-    // console.log("dragging", e);
+    draggedTask = task;
   });
 });
 
@@ -17,8 +18,13 @@ function addDragEventOnColumn(column) {
   column.addEventListener("dragleave", (e) => {
     column.classList.remove("hover-over");
   });
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  })
   column.addEventListener("drop", (e) => {
     e.preventDefault();
+    column.appendChild(draggedTask);
+    column.classList.remove("hover-over");
   });
 }
 
