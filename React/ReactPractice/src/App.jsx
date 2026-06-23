c0f3a4edd392646d35e9eb57ab56c0c89313cba1
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-23 22:21:46 +0530

    Initialize React project with Vite setup, create main components, and add styling

--- React/ReactPractice/src/App.jsx
+++ React/ReactPractice/src/App.jsx
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 const App = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen bg-blue-300">
         <Button />
       </div>
     </>
