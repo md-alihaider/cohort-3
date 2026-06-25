ebbe30ffe80a006d46966adc779921afdfb85aab
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-25 23:24:19 +0530

    feat: implement ProductCard component and update App component layout

--- React/ReactPractice/src/App.jsx
+++ React/ReactPractice/src/App.jsx
@@ -1,10 +1,12 @@
-import Button from "./components/Button";
+// import Button from "./components/Button";
+import ProductCard from "./components/ProductCard";
 
 const App = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-blue-300">
-        <Button />
+        {/* <Button /> */}
+        <ProductCard />
       </div>
     </>
   );
---
+++ React/ReactPractice/src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+
+const ProductCard = () => {
+  return (
+    <div className=" h-3/6 w-3/6 bg-white rounded-xl shadow-md p-5">
+      <div className="w-full h-[70%] bg-gray-200"></div>
+      <div className="w-full h-[30%] bg-gray-800"></div>
+    </div>
+  )
+}
+
+export default ProductCard
