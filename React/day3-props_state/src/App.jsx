cb33fdfa0b6393820fd9dd4dfa7dc8d9a33735bf
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-25 21:21:41 +0530

    feat: add test module with named exports for One, Two, and Four

--- React/day3-props_state/src/App.jsx
+++ React/day3-props_state/src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Contacts from './components/Contacts'
+import something, {OneF, Two} from './test'
 
 const App = () => {
+  something(); // This is default export
+
+  OneF(); // this is named export and will give error as it should be named same as file name
   return (
     <div>
       <Contacts />
---
+++ React/day3-props_state/src/test.jsx
@@ -0,0 +1,14 @@
+export const One = () => {
+  console.log("one");
+}
+export const Two = () => {
+  console.log("two");
+}
+const Three = () => {
+  console.log("three");
+}
+export const Four = () => {
+  console.log("four");
+}
+
+export default Three
