c6de6ab848d47a0bd954c12bcd55be58c871656f
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-25 21:55:18 +0530

    fix: correct formatting and add missing semicolons in App component

--- React/day3-props_state/src/App.jsx
+++ React/day3-props_state/src/App.jsx
@@ -6,26 +6,27 @@ const App = () => {
   //something(); // This is default export
 
   // OneF(); // this is named export and will give error as it should be named same as file name
-  
-  const [count, setCount] = useState(0) // this useState is a hook and it remembers the state of the component if we normaly use it in a function it will not rerender the component
 
+  const [count, setCount] = useState(0); // this useState is a hook and it remembers the state of the component if we normaly use it in a function it will not rerender the component
+
+  // Hooks are special magical tools in React that help your website components "remember" things and do cool tricks behind the scenes
   function increment() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   function decrement() {
     if (count > 0) {
-      setCount(count - 1)
+      setCount(count - 1);
     }
   }
 
   return (
     <div>
-      <h1> Count is  {count}</h1>
+      <h1> Count is {count}</h1>
       <button onClick={() => increment()}>Increment</button>
       <button onClick={() => decrement()}>Decrement</button>
     </div>
-  )
+  );
 }
 
 export default App
