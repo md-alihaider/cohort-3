f884e30f7e02dd1bd170e535e66f83914acbe59d
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-25 21:28:24 +0530

    feat: implement basic layout and counter functionality in App component

--- React/day3-props_state/src/App.jsx
+++ React/day3-props_state/src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Contacts from './components/Contacts'
-import something, {OneF, Two} from './test'
+import React, { useState } from 'react'
+// import Contacts from './components/Contacts'
+// import something, {OneF, Two} from './test'
 
 const App = () => {
-  something(); // This is default export
+  //something(); // This is default export
 
-  OneF(); // this is named export and will give error as it should be named same as file name
+  // OneF(); // this is named export and will give error as it should be named same as file name
+  
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <Contacts />
+      <h1> Count is - {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
     </div>
   )
 }
