0e1db590b7126e70f7aa565c64db56aee2580aa4
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-06-26 20:49:58 +0530

    feat: update ProductCard layout and styling for improved design

--- React/ReactPractice/src/components/ProductCard.jsx
+++ React/ReactPractice/src/components/ProductCard.jsx
@@ -1,11 +1,19 @@
-
 const ProductCard = () => {
   return (
-    <div className=" h-3/6 w-3/6 bg-white rounded-xl shadow-md p-5">
-      <div className="w-full h-[70%] bg-gray-200"></div>
+    <div className=" h-3/6 w-2/5 bg-white rounded-xl shadow-md p-5 flex flex-col ">
+      <div className="w-full h-[70%]  flex">
+        <div className=" h-full w-[45%] bg-gray-400 rounded-xl overflow-hidden">
+          <img
+            src="https://sony.scene7.com/is/image/sonyglobalsolutions/Headphones-product-finder-desktop-and-mobile-Headband-714x439?$productFinder$&fmt=png-alpha"
+            alt="product-image"
+            className=" h-full w-full object-cover"
+          />
+        </div>
+        <div className=" h-full w-[55%] p-5 rounded-xl"></div>
+      </div>
       <div className="w-full h-[30%] bg-gray-800"></div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
