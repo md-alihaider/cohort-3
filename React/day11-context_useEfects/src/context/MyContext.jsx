4f0722e20f7a2847886d5f48074517c0b9ccd6b3
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-07-13 21:18:54 +0530

    fix: correct provider usage and update value prop in ContextProvider

--- React/day11-context_useEfects/src/context/MyContext.jsx
+++ React/day11-context_useEfects/src/context/MyContext.jsx
@@ -2,10 +2,13 @@ import { createContext, useState } from "react";
 
 export const MyStore = createContext();
 
-
 export const ContextProvider = ({ children }) => {
-  console.log("Context rendering")
+  console.log("Context rendering");
   const [count, setCount] = useState(0);
 
-  return <MyStore.ContextProvider val={{count,setCount}}>{ children}</MyStore.ContextProvider>
-}
+  // 1. Changed to MyStore.Provider
+  // 2. Changed val to value
+  return (
+    <MyStore.Provider value={{ count, setCount }}>{children}</MyStore.Provider>
+  );
+};
