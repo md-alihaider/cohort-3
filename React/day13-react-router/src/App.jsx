9f1f26a137393375be513d4ce0b15be6608325ac
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-07-17 20:02:07 +0530

    feat: implement routing with NavLink and update main rendering structure

--- React/day13-react-router/src/App.jsx
+++ React/day13-react-router/src/App.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import { NavLink, Route, Routes } from "react-router";
 
 const App = () => {
-  const [toggle, setToggle] = useState("Home");
+  // const [toggle, setToggle] = useState("Home");
   return (
     <div className="h-screen p-2 text-xl bg-blue-950 text-white">
       <nav className=" flex items-center justify-between mb-2 ">
         <h1>logo</h1>
         <div className=" flex items-center justify-between gap-10 cursor-pointer">
-          <p onClick={() => setToggle("Home")}>Home</p>
-          <p onClick={() => setToggle("About")}>About</p>
-          <p onClick={() => setToggle("Contact")}>Contact</p>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/contact"}>Contact</NavLink>
         </div>
         <button>Login</button>
       </nav>
-      <div>
-        {toggle === "Home" && <Home />}
-        {toggle === "About" && <About />}
-        {toggle === "Contact" && <Contact />}
-      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
