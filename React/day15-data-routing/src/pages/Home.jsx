6844cb9a52f19fc09b97bb42d5486aff0c78cad2
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-07-21 19:37:34 +0530

    feat: implement routing with Home, About, and Services pages

---
+++ React/day15-data-routing/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='h-screen bg-blue-200 flex justify-center items-center text-2xl font-bold'>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
--- React/day15-data-routing/src/routes/AppRoutes.jsx
+++ React/day15-data-routing/src/routes/AppRoutes.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Services from '../pages/Services';
 
 const AppRoutes = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/about",
+      element: <About/>
+    },
+    {
+      path: "/services",
+      element:<Services/>
+    },
+  ])
   return (
-    <div>Data Routing started</div>
+    <RouterProvider router={router}/>
   )
 }
 
