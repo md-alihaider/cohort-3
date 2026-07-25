155b0b5362f2d25ce733795044a51ec60b632156
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-07-25 21:18:05 +0530

    feat: implement AddTodo component with input handling and dispatching addTodo action

--- Redux/reduxToolkitTodo/src/components/AddTodo.jsx
+++ Redux/reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
 
 const AddTodo = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+    dispatch(addTodo(input));
+    setInput("")
+  };
   return (
     <div className="bg-white rounded-2xl shadow-lg border border-gray-200 p-6">
       <h2 className="text-2xl font-bold text-gray-800 mb-2">Add Todo</h2>
@@ -9,13 +20,15 @@ const AddTodo = () => {
         Create a new task to stay organized.
       </p>
 
-      <form className="space-y-5">
+      <form onSubmit={submitHandler} className="space-y-5">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-2">
             Todo
           </label>
 
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
             placeholder="Enter your todo..."
             className="w-full rounded-xl border border-gray-300 px-4 py-3 outline-none transition focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
--- Redux/reduxToolkitTodo/src/features/todo/todoSlice.js
+++ Redux/reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -2,6 +2,7 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
   todos: [{ id: 1, text: "Hello Redux" }],
+  editTodo: null,
 };
 
 export const todoSlice = createSlice({
@@ -19,13 +20,15 @@ export const todoSlice = createSlice({
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
 
-    updateTodo: (state, action) => {
-      const { id, text } = action.payload;
-      const existingTodo = state.todos.find((todo) => todo.id === id);
-      if (existingTodo) {
-        existingTodo.text = text;
-      }
+    editTodo: (state, action) => {
+      state.editTodo = action.payload;
     },
+
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload
+      const existingTodo = state.todos.find((todo) => todo.id === id)
+      existingTodo.text = text
+    }
   },
 });
 
