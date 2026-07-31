95c86a4ed7a67fde81829f3f3f8d5a66825deadb
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-07-31 20:40:01 +0530

    feat: refactor ShopPage to use TanStack Query for data fetching and remove local state management

--- React/day21-project_with_tanstack-query/src/main.jsx
+++ React/day21-project_with_tanstack-query/src/main.jsx
@@ -1,14 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRoutes from './routes/AppRoutes.jsx'
-import {Provider} from "react-redux"
-import { store } from './app/store.js';
-import { ToastContainer } from 'react-toastify';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRoutes from "./routes/AppRoutes.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <AppRoutes />
-    <ToastContainer/>
-  </Provider>,
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <AppRoutes />
+      <ToastContainer />
+    </Provider>
+  </QueryClientProvider>,
 );
--- React/day21-project_with_tanstack-query/src/pages/ShopPage.jsx
+++ React/day21-project_with_tanstack-query/src/pages/ShopPage.jsx
@@ -1,31 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ProductCard from "../components/ProductCard";
 import ProductCardSkeleton from "../components/ProductCardSkeleton";
 import { getProductsDataApi } from "../api/productApi";
+import { useQuery } from "@tanstack/react-query";
 const ShopPage = () => {
-  const [productsData, setProductsData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  
+  const { data, isPending, error } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProductsDataApi,
+    staleTime: 5000,
+  });
 
-  const getData = async() => {
-    const data = await getProductsDataApi();
-    setProductsData(data)
-    setIsLoading(false)
-  }
-  useEffect(() => {
-    getData()
-  },[])
-
+  if (error) return <h1> {error.message} </h1>;
 
   return (
     <div>
       <h1>This is a Shop Page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-        {isLoading
+        {isPending
           ? Array.from({ length: 8 }).map((_, index) => (
               <ProductCardSkeleton key={index} />
             ))
-          : productsData.map((product) => (
+          : data.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
       </div>
