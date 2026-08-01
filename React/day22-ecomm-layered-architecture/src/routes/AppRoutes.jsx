67351c738bd661d459495a53a2b9f72ce5af609f
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-01 14:28:20 +0530

    feat: add AuthLayout, MainLayout, and AppRoutes components; set up Redux store with authSlice

---
+++ React/day22-ecomm-layered-architecture/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const AppRoutes = () => {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:
+    }
+  ])
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
