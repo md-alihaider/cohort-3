7d9d2b85be9e22d130d6afd4ab057c7a9a9e2070
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-08 11:59:55 +0530

    feat: enhance infinite scrolling with pagination and load more functionality in Infinite component

--- React/day25-pagination-infinite-scroll/src/Infinite.jsx
+++ React/day25-pagination-infinite-scroll/src/Infinite.jsx
@@ -1,26 +1,42 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAllProducts } from "./api/productApi";
+import ProductCard from "./components/ProductCard";
 
 const Infinite = () => {
-  const limit = 10;
-  const { data, isPending } = useInfiniteQuery({
-    queryKey: ["products"],
-    queryFn: ({ pageParam }) => getAllProducts(limit, pageParam),
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, allPage) => {
-      let loadedData = lastPage * 10;
-      return loadedData;
-    },
-  });
+  const limit = 40;
+  const { data, isPending, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["products"],
+      queryFn: ({ pageParam }) => getAllProducts(limit, pageParam),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPage) => {
+        console.log(allPage);
+        let loadedData = allPage.length * limit;
+        if (loadedData < lastPage.total) return loadedData;
+        return undefined;
+      },
+    });
 
   if (isPending) return "Loading...";
   console.log(data);
 
+  let allProducts = data?.pages?.flatMap((val) => val.products) ?? [];
+
   return (
-    <div className="flex flex-col p-2 gap-6 items-center text-white bg-black">
+    <div className="flex flex-col p-5 gap-6 items-center text-white bg-black">
       <div className=" w-full p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        Product list goes here
+        {allProducts.map((val) => (
+          <ProductCard key={val.id} product={val} />
+        ))}
       </div>
+      {hasNextPage && (
+        <button
+          onClick={() => fetchNextPage()}
+          className="px-5 py-2 rounded-xl bg-amber-500 hover:bg-amber-600 active:scale-95 transition-all duration-200 text-white font-semibold shadow-lg"
+        >
+          {isFetchingNextPage ? "Loading.." : "Load more →"}
+        </button>
+      )}
     </div>
   );
 };
