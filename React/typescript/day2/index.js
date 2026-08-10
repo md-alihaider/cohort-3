e1e4225e23a43dbb212252fa4ffcb181617833e1
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-10 21:39:16 +0530

    feat: update TypeScript annotations and add array and tuple examples in index.ts

--- React/typescript/day2/index.js
+++ React/typescript/day2/index.js
@@ -5,20 +5,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // a = 98
 // a="88"
 // console.log(a)
-let a = "num";
-a = "98"; //Type 'number' is not assignable to type 'string'
-console.log(a);
+// let a: string = "num";
+// a = "98"; //Type 'number' is not assignable to type 'string'
+// console.log(a);
 //Primitive data Types
 // string
-let val = "Hello";
-// Boolean
-let flag = true;
-// number
-let num = 90;
-// undefined
-let h = undefined;
-// bigint
-let g = 353535n;
-// symbol
-let y = Symbol("hello");
+// let val: string = "Hello";
+// // Boolean
+// let flag: boolean = true;
+// // number
+// let num: number = 90;
+// // undefined
+// let h: undefined = undefined;
+// // bigint
+// let g: bigint = 353535n;
+// // symbol
+// let y: symbol = Symbol("hello");
+console.log("Array and Tuples");
+//Array
+let arr = [1, 2, 45, "adk", undefined, true, null];
+let arr1 = [1, 2, 3, 3];
+let arr2 = ["apple", "banana", "grapes"];
+let arr3 = [true, false, true];
+//Tuples
+let arrtuple = [54, 53]; // In tuples src and target element should be same
 //# sourceMappingURL=index.js.map
--- React/typescript/day2/index.ts
+++ React/typescript/day2/index.ts
@@ -5,25 +5,36 @@
 // a="88"
 // console.log(a)
 
-let a: string = "num";
-a = "98"; //Type 'number' is not assignable to type 'string'
-console.log(a);
+// let a: string = "num";
+// a = "98"; //Type 'number' is not assignable to type 'string'
+// console.log(a);
 
 //Primitive data Types
 // string
-let val: string = "Hello";
+// let val: string = "Hello";
 
-// Boolean
-let flag: boolean = true;
+// // Boolean
+// let flag: boolean = true;
 
-// number
-let num: number = 90;
+// // number
+// let num: number = 90;
 
-// undefined
-let h: undefined = undefined;
+// // undefined
+// let h: undefined = undefined;
 
-// bigint
-let g: bigint = 353535n;
+// // bigint
+// let g: bigint = 353535n;
 
-// symbol
-let y: symbol = Symbol("hello");
+// // symbol
+// let y: symbol = Symbol("hello");
+
+
+console.log("Array and Tuples")
+//Array
+let arr: any[] = [1, 2, 45, "adk", undefined, true, null]
+let arr1:number[] = [1,2,3,3]
+let arr2: string[] = ["apple", "banana", "grapes"]
+let arr3:boolean[]=[true,false,true]
+
+//Tuples
+let arrtuple :[number,number]=[54,53] // In tuples src and target element should be same
