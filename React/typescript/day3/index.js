02ebc4b05ff8782c2d8218420bf599cccc5301e3
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-11 21:23:59 +0530

    refactor: comment out user object type and example, update sum function implementation

--- React/typescript/day3/index.js
+++ React/typescript/day3/index.js
@@ -1,15 +1,31 @@
 "use strict";
+// type userobj = {
+//   name: string;
+//   age: number;
+//   company: string;
+//   address: {
+//     street: string;
+//     city: string;
+//     state: string;
+//   };
+//   employeeId?:string
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
-let obj = {
-    name: "Ali",
-    age: 24,
-    company: "Sheriyans",
-    address: {
-        street: "Mojahidpur",
-        city: "Bhagalpur",
-        state: "Bihar",
-    },
+// let obj: userobj = {
+//   name: "Ali",
+//   age: 24,
+//   company: "Sheriyans",
+//   address: {
+//     street: "Mojahidpur",
+//     city: "Bhagalpur",
+//     state: "Bihar",
+//   },
+// };
+// obj.name = 90;
+// console.log(obj);
+const sum = (a, b) => {
+    return a + b;
 };
-obj.name = 90;
-console.log(obj);
+let res = sum(8, 9);
+console.log(res);
 //# sourceMappingURL=index.js.map
--- React/typescript/day3/index.ts
+++ React/typescript/day3/index.ts
@@ -1,25 +1,33 @@
-type userobj = {
-  name: string;
-  age: number;
-  company: string;
-  address: {
-    street: string;
-    city: string;
-    state: string;
-  };
-  employeeId?:string
-};
+  // type userobj = {
+  //   name: string;
+  //   age: number;
+  //   company: string;
+  //   address: {
+  //     street: string;
+  //     city: string;
+  //     state: string;
+  //   };
+  //   employeeId?:string
+  // };
 
-let obj: userobj = {
-  name: "Ali",
-  age: 24,
-  company: "Sheriyans",
-  address: {
-    street: "Mojahidpur",
-    city: "Bhagalpur",
-    state: "Bihar",
-  },
-};
+  // let obj: userobj = {
+  //   name: "Ali",
+  //   age: 24,
+  //   company: "Sheriyans",
+  //   address: {
+  //     street: "Mojahidpur",
+  //     city: "Bhagalpur",
+  //     state: "Bihar",
+  //   },
+  // };
 
-obj.name = 90;
-console.log(obj);
+  // obj.name = 90;
+// console.log(obj);
+  
+const sum = (a:number, b:number):number => {
+  return a + b;
+}
+
+let res:number = sum(8, 9)
+
+console.log(res)
