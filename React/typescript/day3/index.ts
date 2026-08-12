cb53c2acb0ba327c0115c7f3a3bd23c524c08b90
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-12 13:21:08 +0530

    refactor: comment out sum function implementations and related console logs

--- React/typescript/day3/index.ts
+++ React/typescript/day3/index.ts
@@ -24,10 +24,25 @@
   // obj.name = 90;
 // console.log(obj);
   
-const sum = (a:number, b:number):number => {
-  return a + b;
-}
+// const sum = (a:number, b:number):number => {
+//   return a + b;
+// }
 
-let res:number = sum(8, 9)
+// let res:number = sum(8, 9)
 
-console.log(res)
+// console.log(res)
+
+// const sum = (a: number) => (b: number)=>{
+//   if (b !== undefined) return sum(a + b)
+//   return a
+// }
+
+//Curriying fn
+// const sum = (a: number) => {
+//   return (b: number) => {
+//     if (b !== undefined) return sum(a + b);
+//     return a;
+//   };
+// }
+// let data = sum(7)(7)(8)(8)()
+// console.log(data)
