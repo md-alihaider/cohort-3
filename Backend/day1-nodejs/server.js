e31bcb0eacc490529954b29b233e3dc0c47eeadb
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-15 20:17:22 +0530

    feat: implement basic HTTP server with response handling

--- Backend/day1-nodejs/server.js
+++ Backend/day1-nodejs/server.js
@@ -1,1 +1,11 @@
-console.log("Hello Backend")
+let http = require("http")
+
+
+let server = http.createServer((req, res) => {
+  console.log("Hello i am from Server")
+  res.end("I am server and its working fine")
+})
+
+server.listen(3000, () => {
+  console.log("Server is running on port: 3000..")
+})
