db52c87dab78075eabe9590f15729c601feccf94
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-08-16 19:47:35 +0530

    feat: enhance hero section with loader and animation setup

--- GSAP/learnig-gsap/src/main.js
+++ GSAP/learnig-gsap/src/main.js
@@ -1,38 +1,117 @@
 import "./style.css";
 import { gsap } from "gsap";
-const play = document.getElementById("play");
-const pause = document.getElementById("pause");
-const restart = document.getElementById("restart");
-const tl = gsap.timeline({paused: true});
+// const play = document.getElementById("play");
+// const pause = document.getElementById("pause");
+// const restart = document.getElementById("restart");
+// const reverse = document.getElementById("reverse");
 
-// this is timeline sequencing
-tl.to(".box1", {
-  x: 600,
-  duration: 1.3,
-  ease: "power3.out",
-})
-  .to(".box2", {
-    x: 600,
-    duration: 1.3,
-    ease: "power3.out",
-  })
-  .to(".box3", {
-    x: 600,
-    duration: 1.3,
-    ease: "power3.out",
-  })
-  .to(".box4", {
-    x: 600,
-    duration: 1.3,
-    ease: "power3.out",
-  });
+// const tl = gsap.timeline({paused: true});
 
-play.addEventListener("click", () => {
-  tl.play();
+// // this is timeline sequencing
+// tl.to(".box1", {
+//   x: 600,
+//   duration: 1.3,
+//   ease: "power3.out",
+// })
+//   .to(".box2", {
+//     x: 600,
+//     duration: 1.3,
+//     ease: "power3.out",
+//   })
+//   .to(".box3", {
+//     x: 600,
+//     duration: 1.3,
+//     ease: "power3.out",
+//   })
+//   .to(".box4", {
+//     x: 600,
+//     duration: 1.3,
+//     ease: "power3.out",
+//   });
+
+// play.addEventListener("click", () => {
+//   tl.play();
+// });
+// pause.addEventListener("click", () => {
+//   tl.pause();
+// })
+// restart.addEventListener("click", () => {
+//   tl.restart();
+// })
+// reverse.addEventListener("click", () => {
+//   tl.reverse();
+// })
+
+//repeat
+
+// gsap.to(".box", {
+//   x: 600,
+//   duration: 1.3,
+//   ease: "bounce.In",
+//   repeat: -1,
+//   yoyo: true,
+// })
+
+const obj = {
+  value: 0,
+};
+
+const counter = document.querySelector(".loader-count h2");
+
+gsap.to(obj, {
+  value: 100,
+  duration: 1.7,
+  ease: "none",
+  onUpdate: () => {
+    counter.textContent = `${obj.value.toFixed(0)}%`;
+  },
+  onComplete: () => {
+    gsap.to(counter, {
+      opacity: 0,
+      duration: 1.2,
+      ease: "power3.out",
+      onComplete: () => {
+        tl.play();
+      },
+    });
+  },
 });
-pause.addEventListener("click", () => {
-  tl.pause();
+
+const tl = gsap.timeline({ paused: true });
+
+gsap.set([".heading h1", ".sub-heading p"], {
+  yPercent: 110,
+});
+
+tl.to(".loader", {
+  yPercent: 100,
+  duration: 1.2,
+  ease: "expo.out",
 })
-restart.addEventListener("click", () => {
-  tl.restart();
-})
+  .from(
+    ".hero-bg img",
+    {
+      scale: 1.5,
+      duration: 1.23,
+      ease: "expo.out",
+    },
+    "-=1.1",
+  )
+  .to(
+    ".heading h1",
+    {
+      yPercent: 0,
+      duration: 1.2,
+      ease: "power3.out",
+    },
+    "-=0.9",
+  )
+  .to(
+    ".sub-heading p",
+    {
+      yPercent: 0,
+      duration: 1.2,
+      ease: "power3.out",
+    },
+    "-=0.8",
+  );
