e5b3c303a6dcee132aa736627b11ac87c0a2981e
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-02 19:57:36 +0530

    feat: implement user registration endpoint with JWT token generation

--- Backend/day11-authentication/src/app/app.js
+++ Backend/day11-authentication/src/app/app.js
@@ -1,13 +1,43 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import jwt from "jsonwebtoken";
+const app = express();
+app.use(express.json());
 
-
-app.get('/api',(req,res)=>{
+app.get("/api", (req, res) => {
   res.status(200).json({
-    message:'Welcome to the authentication API'
-  })
-})
+    message: "Welcome to the authentication API",
+  });
+});
 
+app.post("/api/auth/register", (req, res) => {
+  //step 1 get payload form user
+  const { name, email, password } = req.body;
 
+  //step 2 store in db
+  // save data to db
 
-export default app
+  //step 3 generate token
+  const token = jwt.sign(
+    {
+      email,
+      name,
+      // _id
+    },
+    //jwt secret
+    "05536170cd7918fa8190d04b7eaf748a62817b20f0d5271d03607462bd55f6aac8c92dcc5e695e0e70b059ecd763773f",
+  );
+
+  //step 4 send response to user
+  res.status(201).json({
+    message: "User Created Successfully",
+    data: {
+      user: {
+        email,
+        name,
+      },
+      token,
+    },
+  });
+});
+
+export default app;
