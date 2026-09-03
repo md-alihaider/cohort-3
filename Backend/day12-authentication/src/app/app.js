4ce076bdd4b4e22578f7216f9eaf3d3f3c3c7fa7
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-03 18:08:53 +0530

    feat: add user login functionality with password hashing and JWT token generation

--- Backend/day12-authentication/src/app/app.js
+++ Backend/day12-authentication/src/app/app.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -22,7 +24,7 @@ app.post("/api/auth/register", async (req, res) => {
   const user = await userModel.create({
     email,
     name,
-    password,
+    password: await bcrypt.hash(password, 10), //hash the password
   });
 
   //step 3 generate token
@@ -55,6 +57,49 @@ app.get("/api/auth/me", authenticate, async (req, res) => {
     data: {
       user: req.user,
     },
+  });
+});
+
+app.post("/api/auth/login", async (req, res) => {
+  //read payload from req
+  const { email, password } = req.body;
+
+
+  //find user based on email
+  const user = await userModel.findOne({
+    email,
+  });
+
+  //check is password valid 
+  const isValidPassword = bcrypt.compare(password, user.password);
+
+
+  //send res if not valid password
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Invalid Email or Password",
+    });
+  }
+
+  //create login token
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+
+  //send res 
+  res.status(200).json({
+    message: "User Logged in Successfully",
+    data: {
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+    },
+    token,
   });
 });
 
