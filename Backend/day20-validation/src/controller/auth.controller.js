1d7587ac29856c99aadb79d0d5ef20402170f8b9
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-19 11:42:28 +0530

    feat: enhance user registration validation with email, phone, and password checks

--- Backend/day20-validation/src/controller/auth.controller.js
+++ Backend/day20-validation/src/controller/auth.controller.js
@@ -3,4 +3,50 @@
  */
 export const register = async (req, res) => {
   const { email, phone, password } = req.body;
+
+  const errors = [];
+
+  if (!email) {
+    errors.push({
+      field: "email",
+      message: "Email is Required",
+    });
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (email && !emailRegex.test(email)) {
+    errors.push({
+      field: "email",
+      message: "Invalid Email Adress",
+    });
+  }
+
+  if (!phone) {
+    errors.push({
+      field: "phone",
+      message: "Phone Number is Required",
+    });
+  }
+
+  const phoneRegex = /^(?:\+91|91|0)?[ -]?[6-9]\d{9}$/;
+  if (phone && !phoneRegex.test(phone)) {
+    errors.push({
+      field: "phone",
+      message: "Invalid Phone Number",
+    });
+  }
+
+  if (!password && !password.trim()) {
+    errors.push({
+      field: "password",
+      message: "Password is Required",
+    });
+  }
+
+  if (password.trim().length < 6) {
+    errors.push({
+      field: "password",
+      message: "Password must contain minimum 6 character",
+    });
+  }
 };
