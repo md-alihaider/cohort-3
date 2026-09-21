bb61bbb7fe1627a87ebd2fca43d171ba15b33fbf
Md Ali Haider <alihaiderbgp85@gmail.com>
2026-09-21 21:29:19 +0530

    feat: add product model with schema definition for title, description, images, price, sizes, and seller

---
+++ Backend/day21-snitch-project/server/src/models/product.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 20,
+    maxLength: 500,
+  },
+
+  images: {
+    type: [
+      {
+        type: String,
+      },
+    ],
+    validate: {
+      validator: (images) => images.length <= 5,
+      message: "A product can have 5 images allowed",
+    },
+    required: true,
+  },
+  price: {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+      enum: ["INR", "USD"],
+      default: "INR",
+    },
+  },
+  sizes: [
+    {
+      size: {
+        type: String,
+        enum: ["XS", "S", "M", "L", "XL", "XXL"],
+        required: true,
+      },
+      stock: {
+        type: Number,
+        min: 0,
+        default: 0,
+      },  
+    }
+  ],
+  seller: {
+    type: mongoose.Types.ObjectId,
+    ref: "users",
+    required: true,
+  }
+});
